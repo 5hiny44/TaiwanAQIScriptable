e29d73495a0e5c9c088f168b965d0720f4a54c69
蕭安億 <xiaoanyi@xiaoanyis-MacBook-Air.local>
2023-04-21 12:47:23 +0800

    edit 51~100 dark color.

--- TaiwanAQIScriptable.js
+++ TaiwanAQIScriptable.js
@@ -1,7 +1,7 @@
 
 const API_URL = "https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&format=JSON";
 let demoData = {"records": [{"so2":"0.9","status":"普通","pm2.5":"21","pm10_avg":"35","siteid":"23","no2":"12.4","latitude":"24.74091408","wind_speed":"0.2","wind_direc":"89","o3_8hr":"60","sitename":"竹東","pollutant":"臭氧八小時","o3":"43.4","pm10":"35","nox":"13.1","no":"0.6","pm2.5_avg":"19","longitude":"121.08895493","county":"新竹縣","aqi":"67","co":"0.43","so2_avg":"0","publishtime":"2023/04/03 19:00:00","co_8hr":"0.3"}]}
-const aqi = 67
+const aqi = 90
 
 // await getData()
 // test json data
@@ -147,8 +147,8 @@ function aqiBackgroundColor(aqi) {
         [100, {
             lightTopColorCode: "#c7cc35", 
             lightBottomColorCode: "#f2b844",
-            darkTopColorCode: "#4d4d02",
-            darkBottomColorCode: "#664705"
+            darkTopColorCode: "#a2a624",
+            darkBottomColorCode: "#bf8c26"
         }],
         [150, {
             lightTopColorCode: "#e3a00e", 
