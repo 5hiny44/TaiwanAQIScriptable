b1d6e59de07f3b0ce78f4523277448029bc6efe0
蕭安億 <xiaoanyi@xiaoanyis-MacBook-Air.local>
2023-04-29 16:06:44 +0800

    Change 'let' to 'const'.

--- TaiwanAQIScriptable.js
+++ TaiwanAQIScriptable.js
@@ -1,6 +1,6 @@
 
 const API_URL = "https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&format=JSON";
-let demoData = {"records": [{"so2":"0.9","status":"普通","pm2.5":"21","pm10_avg":"35","siteid":"23","no2":"12.4","latitude":"24.74091408","wind_speed":"0.2","wind_direc":"89","o3_8hr":"60","sitename":"竹東","pollutant":"臭氧八小時","o3":"43.4","pm10":"35","nox":"13.1","no":"0.6","pm2.5_avg":"19","longitude":"121.08895493","county":"新竹縣","aqi":"67","co":"0.43","so2_avg":"0","publishtime":"2023/04/03 19:00:00","co_8hr":"0.3"}]}
+const demoData = {"records": [{"so2":"0.9","status":"普通","pm2.5":"21","pm10_avg":"35","siteid":"23","no2":"12.4","latitude":"24.74091408","wind_speed":"0.2","wind_direc":"89","o3_8hr":"60","sitename":"竹東","pollutant":"臭氧八小時","o3":"43.4","pm10":"35","nox":"13.1","no":"0.6","pm2.5_avg":"19","longitude":"121.08895493","county":"新竹縣","aqi":"67","co":"0.43","so2_avg":"0","publishtime":"2023/04/03 19:00:00","co_8hr":"0.3"}]}
 const aqi = 90
 
 // await getData()
@@ -47,68 +47,68 @@ function createWidget(size = config.widgetFamily) {
   
 function createSamllWidget() {
 
-    let widget = new ListWidget()
+    const widget = new ListWidget()
     
     widget.backgroundGradient = aqiBackgroundColor(aqi)
     
-    let titleStack = widget.addStack()
+    const titleStack = widget.addStack()
     titleStack.size = new Size(0, 14)
     //   titleStack.setPadding(0, 10, 0, 10)
     titleStack.topAlignContent()
-    let titleText = titleStack.addText("AQI 湖口")
+    const titleText = titleStack.addText("AQI 湖口")
     titleText.font = Font.boldSystemFont(12)
     titleText.leftAlignText()
     titleText.textOpacity = 0.7
     titleStack.addSpacer()
-    let locationText = titleStack.addText("☀️")
+    const locationText = titleStack.addText("☀️")
     locationText.font = Font.boldSystemFont(12)
     locationText.rightAlignText()
 
     //   widget.addSpacer()
 
-    let aqiStack = widget.addStack()
+    const aqiStack = widget.addStack()
     aqiStack.size = new Size(0, 64)
     
 //     aqiStack.setPadding(0, 100, 0, 0)
     aqiStack.topAlignContent()
-    let aqiText = aqiStack.addText(String(aqi))
+    const aqiText = aqiStack.addText(String(aqi))
 //     aqiText.font = Font.boldSystemFont(60)
     aqiText.font = Font.heavySystemFont(60)
     aqiText.leftAlignText()
 
 //     widget.addSpacer()
     
-    let pollutantStack = widget.addStack()
+    const pollutantStack = widget.addStack()
     pollutantStack.size = new Size(0, 22)
     
     //   pollutantStack.setPadding(0, 10, 0, 10)
     pollutantStack.addSpacer(4)
-    let pollutantText = pollutantStack.addText("PM2.5")
+    const pollutantText = pollutantStack.addText("PM2.5")
     pollutantText.font = Font.boldSystemFont(18)
     pollutantText.leftAlignText()
 //     pollutantStack.addSpacer()
 
 //     widget.addSpacer()
 
-    let otherTitleStack = widget.addStack()
+    const otherTitleStack = widget.addStack()
     otherTitleStack.size = new Size(0, 35)
     //   otherTitleStack.setPadding(0, 10, 0, 10)
 
-    let windStack = otherTitleStack.addStack()
+    const windStack = otherTitleStack.addStack()
 //     windStack.size = new Size(0, 10)
     windStack.bottomAlignContent()
     windStack.layoutVertically()
-    let windText = windStack.addText("東南風 ↖")
+    const windText = windStack.addText("東南風 ↖")
     windText.font = Font.boldSystemFont(11)
     windText.textOpacity = 0.7
 
-    let windText2 = windStack.addText("2.5 m/s")
+    const windText2 = windStack.addText("2.5 m/s")
     windText2.font = Font.boldSystemFont(11)
     windText2.textOpacity = 0.7
 
     otherTitleStack.addSpacer()
 
-    let updateTimeText = otherTitleStack.addText("14:30");
+    const updateTimeText = otherTitleStack.addText("14:30");
     updateTimeText.font = Font.boldSystemFont(11)
     updateTimeText.textOpacity = 0.7
     otherTitleStack.bottomAlignContent()
@@ -220,8 +220,8 @@ function getGradient(lightColorCodes = undefined, darkColorCodes = undefined, lo
   
 async function getData() {
 
-    let req = new Request(API_URL)
-    let json = await req.loadJSON()
+    const req = new Request(API_URL)
+    const json = await req.loadJSON()
 
     try {
         console.log(json)
@@ -231,6 +231,4 @@ async function getData() {
     }
 
 }  
-
-  
   
